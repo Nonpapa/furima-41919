868c783a6ef50811b9fecf7c5dcfca1d97c19b63
mitsu <mitu0790@gmail.com>
2025-03-30 06:23:23 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,31 @@ class Item < ApplicationRecord
   belongs_to :delivery_fee
   belongs_to :prefecture
   belongs_to :shipping_day
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :shipping_day_id
+    validates :price
+  end
+
+  with_options numericality: { other_than: 1, message: 'を選択してください' } do
+    validates :category_id
+    validates :condition_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :shipping_day_id
+  end
+
+  validates :price, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 300,
+    less_than_or_equal_to: 9_999_999,
+    message: 'は¥300〜¥9,999,999の間で入力してください'
+  }
 end
