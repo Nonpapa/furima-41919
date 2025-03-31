99c4e88114cf9142c5ba3a78d2a2f316357dc11c
mitsu <mitu0790@gmail.com>
2025-03-31 19:44:36 +0900

    単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,21 @@
 FactoryBot.define do
   factory :item do
-    name { "MyString" }
-    description { "MyText" }
-    price { 1 }
-    category_id { 1 }
-    status_id { 1 }
-    shipping_fee_status_id { 1 }
-    prefecture_id { 1 }
-    scheduled_delivery_id { 1 }
-    user { nil }
+    association :user
+    name                  { 'テスト商品' }
+    description           { 'テスト説明' }
+    category_id           { 2 }
+    condition_id          { 2 }
+    shipping_fee_status_id { 2 }
+    prefecture_id         { 2 }
+    scheduled_delivery_id { 2 }
+    price                 { 1000 }
+
+    after(:build) do |item|
+      item.image.attach(
+        io: File.open(Rails.root.join('spec/fixtures/test_image.png')),
+        filename: 'test_image.png',
+        content_type: 'image/png'
+      )
+    end
   end
 end
