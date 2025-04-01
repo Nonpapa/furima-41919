9499909a6595f433fab77639ada2afe9b345e02c
mitsu <mitu0790@gmail.com>
2025-04-01 10:13:34 +0900

    last commit

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,23 @@
-window.addEventListener('turbo:load', () => {
+const price = () => {
   console.log("OK");
-});
+
+  const priceInput = document.getElementById("item-price");
+  const addTaxDom = document.getElementById("add-tax-price");
+  const profitDom = document.getElementById("profit");
+
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const tax = Math.floor(inputValue * 0.1);
+    const profit = inputValue - tax;
+
+    addTaxDom.innerHTML = tax;
+    profitDom.innerHTML = profit;
+  });
+
+  console.log(priceInput);  
+  console.log(addTaxDom);   
+  console.log(profitDom); 
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
