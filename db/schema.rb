ae472391fd199550889ec7f092142a4be2b245d1
mitsu <mitu0790@gmail.com>
2025-04-02 11:26:11 +0900

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_29_103643) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_02_011541) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -66,6 +66,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_29_103643) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status_id"
+    t.integer "condition_id", null: false
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
